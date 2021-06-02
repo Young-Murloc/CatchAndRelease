6155a2b35b51d3b56ae8233cfdc07f9c5f669fe9
Young-Murloc <tlswhdgns021@gmail.com>
2021-06-02 16:10:15 +0900

    0602

---
+++ Assets/Script/AnimationScene/controlScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class controlScript : MonoBehaviour
+{
+    public float moveSpeed = 5.0f;
+    float horizontal;
+
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            animator.SetBool("Walk", true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            animator.SetBool("Attack", true);
+        }
+    }
+}
